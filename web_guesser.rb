b23422af14c9bdb778bbd5400709d748d991233e
July Tran <julyytran@gmail.com>
2016-01-25 11:00:51 -0700

    Cheat mode completed

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,12 +5,15 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
+  cheat = params["cheat"]
+  message = check_guess(guess, cheat)
   erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
 
-def check_guess(guess)
-  if guess > SECRET_NUMBER && guess > (SECRET_NUMBER + 5)
+def check_guess(guess, cheat)
+  if cheat == "true"
+    message = "The SECRET NUMBER is #{SECRET_NUMBER}"
+  elsif guess > SECRET_NUMBER && guess > (SECRET_NUMBER + 5)
     message = "Way too high!"
   elsif guess > SECRET_NUMBER
     message = "Too high!"
